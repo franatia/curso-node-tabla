11efce4245fcf0fb94076a7b54d72682ecf3d0f4
Francisco Atia <franciscoatia2108@gmail.com>
2021-12-28 15:39:21 -0300

    Datos agregados

---
+++ notas.js
@@ -0,0 +1,26 @@
+// El archivo .gitignore, sirve para que github sepa que archivos del repositorio
+// no va a leer o agregar en el mismo
+
+// Con los archivos .md, nosotrs podremos agregar notas al repositorio. Uno muy
+// importante es el README, donde damos la explicacion mas fundamental del repositorio
+
+//-------------------CREACION DEL REPOSITORIO LOCALMENTE----------------
+
+// COMANDO 1: git init
+
+// COMANDO 2: git add .
+
+// COMANDO 3: git commit -m "<nombre>"
+
+// OPCIONAL: git checkout --<nombre>
+// Sirve para regenerar el repositorio
+
+// COMANDO 4
+
+//----------------------CREACION DEL REPOSITORIO EN GITHUB------------------
+
+// PASO 1: Creamos el repositorio en github
+
+// PASO 2: Copiamos los comandos de push
+
+// OPCIONAL (EN CASO DE ACTUALIZACION): git push
